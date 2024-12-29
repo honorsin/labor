71e07460894751bd6982bb5884f88f29d91455c8
honorsin <keshawncui@gmail.com>
2024-12-29 22:41:18 +0800

    使用httptest

--- demo/main.go
+++ demo/main.go
@@ -3,57 +3,67 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"syscall/js"
 )
 
 func main() {
 	fmt.Println("WASM Started")
 
+	// 注册 HTTP 路由处理器
+	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]interface{}{
+			"message": "Hello from WASM!",
+			"path":    r.URL.Path,
+		}
+		json.NewEncoder(w).Encode(response)
+	})
+
 	js.Global().Get("labor").Get("http").Set("get", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		fmt.Println("Get handler called")
 		path := args[0].String()
 		fmt.Printf("Path: %s\n", path)
-		return createResponse(path)
-	}))
 
-	fmt.Println("Handler registered")
-	select {}
-}
-
-func createResponse(path string) interface{} {
-	fmt.Println("Creating response")
-	
-	promise := js.Global().Get("Promise").New(js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		resolve := args[0]
-		fmt.Println("Promise executor running")
-
-		// 直接解析 Promise，不使用 goroutine
-		response := map[string]interface{}{
-			"message": "Hello from WASM!",
-			"path":    path,
-		}
-
+		// 创建一个测试请求
+		req := httptest.NewRequest("GET", path, nil)
+		// 创建一个响应记录器
+		rec := httptest.NewRecorder()
+		
+		// 使用 DefaultServeMux 处理请求
+		http.DefaultServeMux.ServeHTTP(rec, req)
+		
+		// 获取响应结果
+		result := rec.Result()
+		
+		// 创建 JS Response 对象
 		jsResponse := js.Global().Get("Object").New()
-		jsResponse.Set("ok", true)
-		jsResponse.Set("status", 200)
-
-		responseBytes, _ := json.Marshal(response)
-		uint8Array := js.Global().Get("Uint8Array").New(len(responseBytes))
-		js.CopyBytesToJS(uint8Array, responseBytes)
-
+		jsResponse.Set("ok", result.StatusCode >= 200 && result.StatusCode < 300)
+		jsResponse.Set("status", result.StatusCode)
+		
+		// 设置 json 方法
 		jsResponse.Set("json", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 			return js.Global().Get("Promise").New(js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 				resolveJson := args[0]
-				resolveJson.Invoke(js.ValueOf(response))
+				
+				var responseData map[string]interface{}
+				if err := json.NewDecoder(result.Body).Decode(&responseData); err != nil {
+					fmt.Printf("Error decoding response: %v\n", err)
+					resolveJson.Invoke(js.ValueOf(map[string]interface{}{
+						"error": err.Error(),
+					}))
+					return nil
+				}
+				
+				resolveJson.Invoke(js.ValueOf(responseData))
 				return nil
 			}))
 		}))
 
-		fmt.Println("Resolving promise")
-		resolve.Invoke(jsResponse)
-		return nil
+		return jsResponse
 	}))
 
-	fmt.Println("Returning promise")
-	return promise
+	fmt.Println("Handler registered")
+	select {}
 }
